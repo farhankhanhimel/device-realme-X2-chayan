6f899c85b6f6726529c3db92c798993f70e7da72
farhankhanhimel <93470433+farhankhanhimel@users.noreply.github.com>
2021-11-02 15:03:08 +0600

    ok

--- extract-files.sh
+++ extract-files.sh
@@ -14,9 +14,9 @@ VENDOR=realme
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-OCTAVI_ROOT="${MY_DIR}"/../../..
+SUPERIOR_ROOT="${MY_DIR}"/../../..
 
-HELPER="${OCTAVI_ROOT}/vendor/octavi/build/tools/extract_utils.sh"
+HELPER="${SUPERIOR_ROOT}/vendor/superior/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -50,7 +50,7 @@ if [ -z "${SRC}" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${OCTAVI_ROOT}" true "${CLEAN_VENDOR}"
+setup_vendor "${DEVICE}" "${VENDOR}" "${SUPERIOR_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
         "${KANG}" --section "${SECTION}"

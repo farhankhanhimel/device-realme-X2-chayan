5b466defdd2ff31b76af460ddb99a32708dd151a
farhankhanhimel <93470433+farhankhanhimel@users.noreply.github.com>
2021-11-02 15:13:12 +0600

    p

--- superior_X2.mk
+++ superior_X2.mk
@@ -7,25 +7,17 @@
 $(call inherit-product, device/realme/X2/device.mk)
 
 # Inherit some common Octavi stuff.
-$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
-
-OCTAVI_BUILD_TYPE=unOfficial
-OCTAVI_DEVICE_MAINTAINER=CHAITANYA
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 WITH_GAPPS := true
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
-# OctaviOS stuff
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := false
-TARGET_WANTS_FOD_ANIMATIONS := true
-
 
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := octavi_X2
+PRODUCT_NAME := superior_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
